050a19405d8de1eb905377e12d46bd0d4dcf8312
Adeury09 <Adeury0909>
2016-10-11 21:14:08 -0400

    Vamos

---
+++ src/prueba/prueba.java
@@ -0,0 +1,28 @@
+package prueba;
+
+public class prueba {
+	static int n = 5;
+	 static int j = 6;
+	 static int sum = n + j;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		printHello();
+		sum();
+		printSum();
+
+	
+		
+	}
+	static String printHello(){
+		return "Hello World";
+	}
+	static int sum(){
+		sum = n+j;
+		return sum;
+	}
+	static String printSum(){
+		return sum+"";
+	}
+
+}
+
---
+++ src/prueba/test.java
@@ -0,0 +1,25 @@
+package prueba;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test {
+
+	@Test
+	public void test() {
+	prueba p = new prueba();
+	assertEquals(90, p.sum());
+	}
+//	public void test2() {
+//		prueba p = new prueba();
+//		assertEquals("90", p.printSum());
+//		
+//		}
+//	public void test() {
+//		prueba p = new prueba();
+//		assertEquals("90", p.printSum());
+//		
+//		}
+
+}
